17f9596083afbe3faba5e9a2a5e9cefd8fe33cac
cristian <cristian982940@gmail.com>
2019-05-17 14:58:56 -0500

    read data of the form

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -2,19 +2,44 @@ import React, {Component} from 'react';
 
 
 export default class Formulario extends Component{
+
+  //crear ref
+  ciudadRef = React.createRef()
+  paisRef = React.createRef()
+
+
+
+  buscarClima = e => {
+    e.preventDefault()
+
+    //Leer Ref y crear el objeto
+    const respuesta = {
+      ciudad: this.ciudadRef.current.value,
+      pais: this.paisRef.current.value
+    }
+
+    console.log(respuesta)
+    //Enviar por props
+
+    //Resetiar el formulario
+
+
+  }
+
   render(){
     return(
       <div className="contenedor-form">
         <div className="container">
           <div className="row">
-            <form>
+
+            <form onSubmit={this.buscarClima}>
               <div className="input-field col s12 m8 l4 offset-m2">
-                <input id="ciudad" type="text" />
+                <input  ref={this.ciudadRef} id="ciudad" type="text" />
                 <label htmlFor="ciudad">Ciudad: </label>
               </div>
 
               <div className="input-field col s12 m8 l4 offset-m2">
-                <select>
+                <select ref={this.paisRef}>
                   <option value="" defaultValue>Elige un pais</option>
                   <option value="AR">Argentina</option>
                   <option value="CO">Colombia</option>
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -5,7 +5,7 @@ const Header = (props) =>{
   return(
     <div>
       <nav className="nav-wrapper ligth-blue darken-2">
-        <a className="brand-logo">{props.titulo}</a>
+        <a href="https://google.com" className="brand-logo">{props.titulo}</a>
       </nav>
     </div>
   )
